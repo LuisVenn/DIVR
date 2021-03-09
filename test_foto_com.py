8281128eeca8ce92b1b87d2bdc1f6c685cc162e3
Luís Venn <up201603895@g.uporto.pt>
2021-03-09 17:39:23 +0000

    take photo tests code

---
+++ test_foto_com.py
@@ -0,0 +1,14 @@
+from paramiko import SSHClient
+import subprocess
+
+client = SSHClient()
+client.load_system_host_keys()
+client.connect('192.168.0.11',22,'pi','divr2021')
+
+return01 = subprocess.Popen(['python3','foto.py'], cwd='/home/pi/Desktop') 
+stdin,stdout,stderr = client.exec_command('cd Desktop;python3 foto.py') 
+
+
+client.close()
+
+print('Feito')
